190fa95ed70f78028a555109daabc11ba7fc22fb
jwywoo <jwywoo26@gmail.com>
2023-09-18 09:58:07 +0900

    5th - 2 Commit(JWYWOO): Opinion domain modified for service and controller

--- src/main/java/com/example/townassembly/domain/post/opinion/dto/OpinionResponseDto.java
+++ src/main/java/com/example/townassembly/domain/post/opinion/dto/OpinionResponseDto.java
@@ -1,11 +1,19 @@
 package com.example.townassembly.domain.post.opinion.dto;
 
+import com.example.townassembly.domain.post.opinion.entity.Opinion;
 import lombok.Getter;
 
 @Getter
 public class OpinionResponseDto {
-    private Long id;
-    private String title;
-    private String content;
-    private String username;
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String username;
+
+    public OpinionResponseDto(Opinion opinion) {
+        this.id = opinion.getId();
+        this.title = opinion.getTitle();
+        this.content = opinion.getContent();
+        this.username = opinion.getUsername();
+    }
 }
--- src/main/java/com/example/townassembly/domain/post/opinion/entity/Opinion.java
+++ src/main/java/com/example/townassembly/domain/post/opinion/entity/Opinion.java
@@ -1,5 +1,7 @@
 package com.example.townassembly.domain.post.opinion.entity;
 
+import com.example.townassembly.domain.post.opinion.dto.OpinionRequestDto;
+import com.example.townassembly.global.entity.Timestamped;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "opinion")
 @NoArgsConstructor
-public class Opinion {
+public class Opinion extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,4 +20,15 @@ public class Opinion {
     private String title;
     @Column(name = "content", nullable = false)
     private String content;
+
+    public Opinion(OpinionRequestDto requestDto, String username) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+        this.username = username;
+    }
+
+    public void update(OpinionRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+    }
 }
