c4dfbf26c66371e94f794bd5f71880f2e7b35dc2
jwywoo <jwywoo26@gmail.com>
2023-09-18 11:36:26 +0900

    7th - 2 Commit(JWYWOO): Complement domain modified for service and controller

--- src/main/java/com/example/townassembly/domain/comment/complement/repository/ComplementRepository.java
+++ src/main/java/com/example/townassembly/domain/comment/complement/repository/ComplementRepository.java
@@ -3,5 +3,8 @@ package com.example.townassembly.domain.comment.complement.repository;
 import com.example.townassembly.domain.comment.complement.entity.Complement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ComplementRepository extends JpaRepository<Complement, Long> {
+    List<Complement> findAllByUsernameOrderByModifiedAtDesc(String username);
 }
