fbe93d760dd0946d45c0819ba46fbe3b3922707b
jwywoo <jwywoo26@gmail.com>
2023-09-21 00:49:03 +0900

    13th - 1 Commit(JWYWOO): Comment Refactored and nickname applied

--- src/main/java/com/example/townassembly/domain/comment/comment/dto/CommentResponseDto.java
+++ src/main/java/com/example/townassembly/domain/comment/comment/dto/CommentResponseDto.java
@@ -6,14 +6,14 @@ import lombok.Getter;
 @Getter
 public class CommentResponseDto {
     private final String title;
-    private final String username;
+    private final String nickname;
     private final String content;
     private final Boolean likeStat;
     private final Integer likeCount;
 
     public CommentResponseDto(Comment comment,Boolean likeStat,Integer likeCount) {
         this.title = comment.getTitle();
-        this.username = comment.getUsername();
+        this.nickname = comment.getNickname();
         this.content = comment.getContent();
         this.likeStat = likeStat;
         this.likeCount = likeCount;
--- src/main/java/com/example/townassembly/domain/comment/comment/dto/CommentResponseDtoList.java
+++ src/main/java/com/example/townassembly/domain/comment/comment/dto/CommentResponseDtoList.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 @Getter
 public class CommentResponseDtoList {
     private final String title;
-    private final String username;
+    private final String nickname;
     private final String content;
 
     public CommentResponseDtoList(Comment comment) {
         this.title = comment.getTitle();
-        this.username = comment.getUsername();
+        this.nickname = comment.getNickname();
         this.content = comment.getContent();
     }
 }
