784bbd21923a06422cfcfe696c4f8ac3c48fa863
jwywoo <jwywoo26@gmail.com>
2023-09-20 11:18:28 +0900

    11th - 4 Commit(JWYWOO): 선택 정치인의 응원댓글 전체 조회 완료(GET /api/complements/{userid})

--- src/main/java/com/example/townassembly/domain/comment/complement/repository/ComplementRepository.java
+++ src/main/java/com/example/townassembly/domain/comment/complement/repository/ComplementRepository.java
@@ -1,10 +1,13 @@
 package com.example.townassembly.domain.comment.complement.repository;
 
+import com.example.townassembly.domain.comment.complement.dto.ComplementResponseDto;
 import com.example.townassembly.domain.comment.complement.entity.Complement;
+import com.example.townassembly.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ComplementRepository extends JpaRepository<Complement, Long> {
     List<Complement> findAllByUsernameOrderByModifiedAtDesc(String username);
+    List<Complement> findAllByForWhomOrderByCreatedAt(User user);
 }
