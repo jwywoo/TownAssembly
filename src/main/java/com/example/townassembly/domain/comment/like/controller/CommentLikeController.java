359df97f360a1cace97c0e867cba08d17d2374ba
jwywoo <jwywoo26@gmail.com>
2023-09-26 10:09:00 +0900

    22nd - 1 Commit(JWYWOO) Refactoring: Response Entity Modified CommentLike

--- src/main/java/com/example/townassembly/domain/comment/like/controller/CommentLikeController.java
+++ src/main/java/com/example/townassembly/domain/comment/like/controller/CommentLikeController.java
@@ -1,7 +1,9 @@
 package com.example.townassembly.domain.comment.like.controller;
 
 import com.example.townassembly.domain.comment.like.service.CommentLikeService;
+import com.example.townassembly.domain.user.entity.User;
 import com.example.townassembly.global.security.UserDetailsImpl;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,6 +20,11 @@ public class CommentLikeController {
     private final CommentLikeService commentLikeService;
     @PostMapping("/comment/like/{id}")
     public void commentLike(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        commentLikeService.commentLike(id,userDetails.getUser());
+        try {
+            User user = userDetails.getUser();
+            commentLikeService.commentLike(id,user);
+        } catch (Exception e) {
+            throw new JwtException(e.getMessage());
+        }
     }
 }
