547b9bba070a35b6d4b1aa6894c20840a9694ab0
jwywoo <jwywoo26@gmail.com>
2023-09-26 15:34:21 +0900

    24th Commit(JWYWOO) Comment Response Dto Bug fixed

--- src/main/java/com/example/townassembly/domain/comment/comment/dto/CommentResponseDto.java
+++ src/main/java/com/example/townassembly/domain/comment/comment/dto/CommentResponseDto.java
@@ -7,17 +7,19 @@ import lombok.Getter;
 
 @Getter
 public class CommentResponseDto {
-    private final String title;
-    private final String nickname;
-    private final String content;
+    private final Long commentId;
+    private final String commentTitle;
+    private final String commentNickname;
+    private final String commentContent;
     private final Boolean likeStat;
     private final Integer likeCount;
     private final UserRoleEnum role;
 
     public CommentResponseDto(Comment comment,Boolean likeStat,Integer likeCount) {
-        this.title = comment.getTitle();
-        this.nickname = comment.getNickname();
-        this.content = comment.getContent();
+        this.commentId = comment.getId();
+        this.commentTitle = comment.getTitle();
+        this.commentNickname = comment.getNickname();
+        this.commentContent = comment.getContent();
         this.likeStat = likeStat;
         this.likeCount = likeCount;
         this.role = comment.getRole();
